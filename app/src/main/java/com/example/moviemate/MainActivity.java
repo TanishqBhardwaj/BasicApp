d230a66bc756a88a48be14335a72137fa7c62a99
MrTan1999 <tanishqbhardwaj.tb@gmail.com>
2019-09-08 15:58:46 +0530

    Fetching API details, and trying to fetch image using Fresco

---
+++ app/src/main/java/com/example/moviemate/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.moviemate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+    final static String apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key=b8f745c2d43033fd65ce3af63180c3c3";
+    private RecyclerView mRecyclerView;
+    private MovieAdapter mMovieAdapter;
+    private ArrayList<MovieItem> mMovieList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fresco.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true); // what does this mean
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this)); // what does this mean
+        mMovieList = new ArrayList<>();
+
+        URL searchURL = buildUrl();
+        new queryTask().execute(searchURL);
+    }
+
+    public class queryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchURL  = urls[0]; // what does this mean
+            String searchResults = null;
+            try {
+                searchResults = getResponseFromHttpUrl(searchURL);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return searchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s!=null && !s.equals("")) {
+                try {
+                    JSONObject ob = new JSONObject(s);
+                    onResponse(ob);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void onResponse(JSONObject response) throws JSONException {
+            String initialImageUrl = "http://image.tmdb.org/t/p/original";
+            JSONArray jsonArray = response.getJSONArray("results");
+
+            for(int i=0; i<jsonArray.length(); i++) {
+                JSONObject result = jsonArray.getJSONObject(i);
+                String imageUrl = initialImageUrl.concat(result.getString("poster_path"));
+                String title = result.getString("title");
+                int popularity = result.getInt("popularity");
+                mMovieList.add(new MovieItem(imageUrl, title, popularity));
+            }
+            mMovieAdapter = new MovieAdapter(MainActivity.this, mMovieList);
+            mRecyclerView.setAdapter(mMovieAdapter);
+        }
+    }
+
+    public static URL buildUrl() {
+        Uri builtUri = Uri.parse(apiUrl);
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            }
+            else {
+                return null;
+            }
+        }
+        finally {
+            urlConnection.disconnect();
+        }
+    }
+
+}
